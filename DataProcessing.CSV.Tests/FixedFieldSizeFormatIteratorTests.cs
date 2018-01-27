107fae03a0e126813ff8ce1461ea1fa1a50aba4d
Maksym Diabin <maksym.diabin@gmail.com>
2018-01-27 18:27:03 +0000

    Created a starter commit with a very simple functionality. Fixed file format. Basic iterator.

---
+++ DataProcessing.CSV.Tests/FixedFieldSizeFormatIteratorTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace DataProcessing.CSV.Tests
+{
+    [TestFixture]
+    public class FixedFieldSizeFormatIteratorTests
+    {
+        const string Data0 = "1234567812345678123412\n";
+
+        [Test]
+        public void Iterator_Only_Cares_About_Fields_Defined()
+        {
+            var iterator = new FixedFieldSizeFormatIterator<string>(Data0, 8, 8, 4, 2);
+            var enumerator = iterator.GetEnumerator();
+
+            enumerator.MoveNext();
+            Assert.AreEqual("12345678", enumerator.Current);
+
+            enumerator.MoveNext();
+            Assert.AreEqual("12345678", enumerator.Current);
+
+            enumerator.MoveNext();
+            Assert.AreEqual("1234", enumerator.Current);
+
+            enumerator.MoveNext();
+            Assert.AreEqual("12", enumerator.Current);
+        }
+    }
+}
---
+++ DataProcessing.CSV/FixedFieldSizeFormatIterator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataProcessing.CSV
+{
+    public class FixedFieldSizeFormatIterator<T> : IEnumerable<string>
+    {
+        private readonly string _row;
+        private readonly int[] _widths;
+        private int _currentIndex = 0;
+
+        public FixedFieldSizeFormatIterator(string row, params int[] widths)
+        {
+            _row = row;
+            _widths = widths;
+        }
+
+        IEnumerator<string> IEnumerable<string>.GetEnumerator()
+        {
+            while (_widths.Length > _currentIndex)
+            {
+                var currentWidth = _widths[_currentIndex++];
+                var currentString = new string(_row.Take(currentWidth).ToArray());
+
+                yield return currentString;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable<string>) this).GetEnumerator();
+        }
+    }
+
+}
